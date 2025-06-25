9296c17399d69d6c9f0c28ac748f71daf06a014e
Age Setia Budi <agesetiabudi16@gmail.com>
2025-06-25 10:03:03 +0700

    FRAME

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="/frame/1"
             target="_blank"
             rel="noopener noreferrer"
           >
